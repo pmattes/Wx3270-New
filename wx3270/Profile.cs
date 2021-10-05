fb7a7ccca4718a7ed292a9ebe6786a6344891ba2
Paul Mattes <paul.mattes@case.edu>
2021-10-05 00:00:21 -0500

    epre

--- wx3270/Profile.cs
+++ wx3270/Profile.cs
@@ -563,7 +563,7 @@ namespace Wx3270
             /// <summary>
             /// Release phase.
             /// </summary>
-            private const string ReleasePhase = "dpre";
+            private const string ReleasePhase = "epre";
 
             /// <summary>
             /// Gets the full version, for display rather than comparison.
