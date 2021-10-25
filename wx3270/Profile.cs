e68040e465a15c5a2b3cfe78a9e8c0aa4fe138dd
Paul Mattes <paul.mattes@case.edu>
2021-10-24 23:30:33 -0500

    gpre6

--- wx3270/Profile.cs
+++ wx3270/Profile.cs
@@ -569,7 +569,7 @@ namespace Wx3270
             /// <summary>
             /// Release phase.
             /// </summary>
-            private const string ReleasePhase = "fpre";
+            private const string ReleasePhase = "gpre";
 
             /// <summary>
             /// Gets the full version, for display rather than comparison.
