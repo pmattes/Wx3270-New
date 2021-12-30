001253bdcb3eba5488f06e5640fb66536796b9b3
Paul Mattes <paul.mattes@case.edu>
2021-12-30 12:59:06 -0600

    1.0ga8

--- wx3270/Release.cs
+++ wx3270/Release.cs
@@ -15,7 +15,7 @@ namespace Wx3270
         /// <remarks>
         /// CHANGE THIS WHENEVER PRE-RELEASE CODE IS CUT, AND MAKE SURE IT IS NULL WHEN A TAGGED RELEASE IS CUT.
         /// </remarks>
-        private const string PreReleaseIteration = "C";
+        private const string PreReleaseIteration = null;
 
         /// <summary>
         /// Release phases.
@@ -44,7 +44,7 @@ namespace Wx3270
         /// <remarks>
         /// CHANGE THIS WHEN THE RELEASE PHASE CHANGES.
         /// </remarks>
-        public static Phases Phase => Phases.Alpha;
+        public static Phases Phase => Phases.GA;
 
         /// <summary>
         /// Gets the name of the release phase.
