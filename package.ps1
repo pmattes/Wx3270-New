24d7c38505da1b147902676e06ca7cbb21a48d76
Paul Mattes <paul.mattes@case.edu>
2025-02-08 18:42:42 -0600

    Switch signing

--- package.ps1
+++ package.ps1
@@ -3,21 +3,17 @@
     [switch]$archive = $false,
     # Commit flag
     [switch]$commit = $false
-
 )
 
 # Set up constants.
 $inno = 'C:\Program Files (x86)\Inno Setup 6\ISCC.exe'
-$cert = 'C:\Users\pdm\Documents\Certs\Cert2022.pfx'
-$signtool = 'C:\Program Files (x86)\Windows Kits\10\App Certification Kit\signtool.exe'
-$timestamp = 'http://timestamp.digicert.com'
 
 # Any error kills the script.
 $ErrorActionPreference = 'Stop'
 
-# Read the password.
-$p = Read-Host 'Enter key password' -AsSecureString
-$pass = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($p))
+# Set up Azure login to do signing.
+Import-Module Az
+Connect-AzAccount | Out-Null
 
 # Build.
 Write-Host -ForegroundColor Green 'Building x86'
@@ -25,14 +21,18 @@ Write-Host -ForegroundColor Green 'Building x86'
 Write-Host -ForegroundColor Green 'Building x64'
 & msbuild wx3270\wx3270.sln /t:Rebuild "/p:Configuration=Release;Platform=x64" /nologo /v:q
 
-# Sign the binaries.
+# Sign the binaries that are not yet signed.
 $exclude = "Newtonsoft.Json.dll"
 $files = `
    (Get-ChildItem -Path Wx3270\bin\x86\Release, Wx3270\bin\x64\Release -Recurse -Filter "*.exe").FullName `
  + (Get-ChildItem -Path Wx3270\bin\x86\Release, Wx3270\bin\x64\Release -Recurse -Filter "*.dll" -Exclude $exclude).FullName `
- + (Get-ChildItem -Path Wx3270Restrict\bin\Release -Recurse -Filter "*.exe").FullName
-Write-Host -ForegroundColor Green 'Signing', $files.Count, 'binaries'
-& $signtool sign /f $cert /p $pass /td SHA256 /tr $timestamp /fd SHA256 $files
+ + (Get-ChildItem -Path Wx3270Restrict\bin\Release -Recurse -Filter "*.exe").FullName |
+   Where-Object { (Get-AuthenticodeSignature $_).Status -eq "NotSigned" }
+if ($files.Count -gt 0)
+{
+    Write-Host -ForegroundColor Green 'Signing', $files.Count, "binaries"
+    & .\run-signtool.ps1 $files
+}
 
 # Figure out the version.
 $vfile = [System.IO.Path]::GetTempFileName()
@@ -47,11 +47,13 @@ Set-Content -Encoding UTF8 -Path tmp.iss -Value $iss
 
 # Run Inno Setup to create the installer.
 Write-Host -ForegroundColor Green 'Running Inno Setup'
-$signparm = '/smystandard="' + "$signtool sign /f `$q$cert`$q /p $pass /td SHA256 /tr $timestamp /fd SHA256 `$p" + '"'
+$cwd = (Get-Location)
+$signparm = '/smystandard="' + "powershell.exe $cwd\run-signtool.ps1 `$p" + '"'
 & $inno $signparm /Qp tmp.iss
 Remove-Item tmp.iss
 
 # Create the no-install zipfiles.
+Write-Host -ForegroundColor Green 'Creating no-install zipfiles'
 $files = Get-Content noinstall-files.txt
 $files.ForEach({$_ -replace '^', 'wx3270\bin\x64\Release\'}) | Compress-Archive -Force -DestinationPath "wx3270-$version-noinstall64.zip"
 $files.ForEach({$_ -replace '^', 'wx3270\bin\x86\Release\'}) | Compress-Archive -Force -DestinationPath "wx3270-$version-noinstall32.zip"
---
+++ run-signtool.ps1
@@ -0,0 +1,58 @@
+﻿# run-signtool.ps1 <files-to-sign>
+
+# Any error kills the script.
+$ErrorActionPreference = 'Stop'
+
+# Find everything.
+$signtool = 'C:\Program Files (x86)\Windows Kits\10\bin\10.0.26100.0\x64\signtool.exe'
+$dlib = 'C:\Users\pdm\AppData\Local\Microsoft\MicrosoftTrustedSigningClientTools\Azure.CodeSigning.Dlib.dll'
+$timestamp = 'http://timestamp.acs.microsoft.com'
+
+# Put the Azure Trusted Signing metadata in a temporary file.
+$tempJson = Get-ChildItem ([IO.Path]::GetTempFileName()) | Rename-Item -NewName { [IO.Path]::ChangeExtension($_, ".json") } -PassThru
+$json = @'
+{
+  "Endpoint": "https://wcus.codesigning.azure.net",
+  "CodeSigningAccountName": "x3270",
+  "CertificateProfileName": "x3270",
+  "ExcludeCredentials": [
+    "AzureCliCredential",
+    "ManagedIdentityCredential",
+    "EnvironmentCredential",
+    "WorkloadIdentityCredential",
+    "SharedTokenCacheCredential",
+    "VisualStudioCredential",
+    "VisualStudioCodeCredential",
+    "AzureDeveloperCliCredential",
+    "InteractiveBrowserCredential"
+  ]
+}
+'@
+$json | Out-File -FilePath $tempJson -Encoding ascii
+
+# Sign, suppressing output.
+$count = 0
+foreach ($file in $args[0])
+{
+    $percent = ($count / $args[0].Count) * 100
+    $remaining = ($args[0].Count - $count) * 5
+    $count = $count + 1
+    Write-Progress -Activity "Signing" -CurrentOperation $file -PercentComplete $percent -SecondsRemaining $remaining
+    $out = (& $signtool sign /v /td SHA256 /tr $timestamp /fd SHA256 /dlib $dlib /dmdf $tempjson $file)
+    if ((Get-AuthenticodeSignature $file).Status -ne "Valid")
+    {
+        Write-Error "$file not signed"
+        Write-Error "Signtool output: $out"
+    }
+}
+Write-Progress -Activity "Signing" -Completed
+Remove-Item $tempJson
+
+# Check.
+$invalid = ($args[0] | Where-Object { (Get-AuthenticodeSignature $_).Status -ne "Valid" })
+if ($invalid.Count -ne 0)
+{
+    # Signtool failed.
+    Write-Error "File(s) were not signed: $invalid"
+    exit 1
+}
